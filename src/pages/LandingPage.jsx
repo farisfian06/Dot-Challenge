6442ae5481096b487e888c224735b5fcf44e5b8b
farisfian06 <farisfian06@gmail.com>
2024-12-28 21:12:56 +0700

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import HeroSection from "../components/sections/landing/HeroSection";
+import HowItWork from "../components/sections/landing/HowItWork";
+import Categories from "../components/sections/landing/Categories";
 
 const LandingPage = () => {
   return (
-    <div className="space-y-4 max-w-md bg-primary justify-center mx-auto p-4">
-      <div className=" ">LandingPage</div>
-      <h1 className="bg-secondary">ntyuaks</h1>
-    </div>
+    <>
+      <HeroSection />
+      <HowItWork />
+      <Categories />;
+    </>
   );
 };
 
