7ab78afce70a889895b9ea1f598152a9218f0de4
farisfian06 <farisfian06@gmail.com>
2024-12-28 21:16:12 +0700

    Update Routes.jsx

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,11 +3,31 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import LandingPage from "../pages/LandingPage";
 import NotFound from "../pages/NotFound";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Dashboard from "../pages/Dashboard";
+import Quiz from "../pages/Quiz";
 
 const createRouter = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/quiz",
+    element: <Quiz />,
   },
   {
     path: "*",
