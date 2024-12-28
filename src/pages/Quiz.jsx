d4451b39c50f04b8f04627d9e7bf1784e35c40df
farisfian06 <farisfian06@gmail.com>
2024-12-28 21:15:46 +0700

    Create Quiz.jsx

---
+++ src/pages/Quiz.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Quiz = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="flex py-4 gap-8 h-screen">
+          <div className="w-[70%] h-full bg-red-400 border-primary border-2 rounded-xl px-4 py-6 ">
+            <h3>Soal</h3>
+          </div>
+          <div className="w-[30%] h-full bg-green-400">a</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Quiz;
