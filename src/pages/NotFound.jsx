ddb2a167ec147e452b9ac1cb187f95b061b45927
farisfian06 <farisfian06@gmail.com>
2024-12-30 17:48:28 +0700

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <section className="bg-gradient-to-tl from-primary to-secondary">
+      <div className="container min-h-screen flex justify-center items-center">
+        <h1 className="text-9xl font-primaryBold">Page Not Found</h1>
+      </div>
+    </section>
+  );
 };
 
 export default NotFound;
