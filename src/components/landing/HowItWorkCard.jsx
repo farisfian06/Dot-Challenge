3ae2b37d356edf9e817dff2f11015413e90ebe36
farisfian06 <farisfian06@gmail.com>
2025-01-02 21:33:35 +0700

    Update HowItWorkCard.jsx

--- src/components/landing/HowItWorkCard.jsx
+++ src/components/landing/HowItWorkCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HowItWorkCard = ({ judul, deskripsi, children }) => {
   return (
-    <div className="flex flex-col items-center w-full p-6 bg-white shadow-lg rounded-lg hover:scale-110 transition-all duration-300 ease-in-out">
+    <div className="flex flex-col items-center w-full h-full p-6 bg-white shadow-lg rounded-lg hover:scale-110 transition-all duration-300 ease-in-out">
       <div className="w-[35%] aspect-square flex justify-center items-center bg-primary text-white text-3xl rounded-full mb-6">
         {children}
       </div>
